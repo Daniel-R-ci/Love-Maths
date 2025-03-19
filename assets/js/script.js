438fe408c95227b35cb6f0d4fb4244e43feee408
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-03-19 21:18:01 +0100

    Add event listener for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,47 @@
-function runGame(){
+// Wait for DOM to finish loading before running the game
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      }
+      else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
+
+function runGame() {
 
 }
 
-function checkAnswer(){
+function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function incrementScore(){
+function incrementScore() {
 
 }
 
-function incrementIncorrectAnswer(){
+function incrementIncorrectAnswer() {
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion() {
 
 }
 
-function displaySubtractQuestion(){
+function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion{
-  
+function displayMultiplyQuestion(){
+
 }
