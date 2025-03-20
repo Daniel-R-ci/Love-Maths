d1ee7cc1404cac12715d74022f300a3710caf71c
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-03-20 09:42:16 +0100

    Calculate correct answer for addition games

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,13 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 function runGame(gameType) {
   //Generate two random numbers between 1 and 25
-  let num1=Math.floor(Math.random()*25)+1;
-  let num2=Math.floor(Math.random()*25)+1;
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
 
-  if(gameType === "addition"){
+  if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
   }
-  else{
+  else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType} Aborting!`;
   }
@@ -37,8 +37,22 @@ function checkAnswer() {
 
 }
 
+/**
+ * Gets the operand (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.getElementById("operand1").innerText);
+  let operand2 = parseInt(document.getElementById("operand2").innerText);
+  let operator = document.getElementById("operator").innerText;
 
+  if (operator === "+") { //Addition game
+    return [operand1, operand2, "addition"];
+  }
+  else {
+    alert(`Unimplemented operator: ${operator}`);
+    throw `Unimplemented operator: ${operator}. Aborting!`;
+  }
 }
 
 function incrementScore() {
@@ -50,16 +64,16 @@ function incrementIncorrectAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-  document.getElementById("operand1").textContent=operand1;
-  document.getElementById("operand2").textContent=operand2;
-  document.getElementById("operator").textContent="+";
-  
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "+";
+
 }
 
 function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion() {
 
 }
